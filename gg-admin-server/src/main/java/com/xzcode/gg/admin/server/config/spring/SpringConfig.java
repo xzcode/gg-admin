77a065b3958dc725a3842baba3e8eb9a3a70a88e
zai <379551875@qq.com>
2020-04-11 11:51:33 +0800

    配置修改

--- gg-admin-server/src/main/java/com/xzcode/gg/admin/server/config/spring/SpringConfig.java
+++ gg-admin-server/src/main/java/com/xzcode/gg/admin/server/config/spring/SpringConfig.java
@@ -5,10 +5,9 @@ import org.springframework.context.annotation.Configuration;
 /**
  * spring 配置
  * 
- * @author zai
- * 2018-12-25 13:45:12
+ * @author zai 2018-12-25 13:45:12
  */
 @Configuration
 public class SpringConfig {
-   
+	
 }
